22ed3e170438df159bad29b3c54947d7910b231e
CreateEverthing <CreateEverthing@todo.com>
2017-10-30 21:17:08 +0800

    spring-jdbc

---
+++ Spring/src/test/java/com/kaishengit/test/JdbcTest.java
@@ -0,0 +1,43 @@
+package com.kaishengit.test;
+
+import com.kaishengit.entity.Book;
+import com.kaishengit.service.BookService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="classpath:applicationContext.xml")
+public class JdbcTest {
+    @Autowired
+    BookService bookService;
+    @Test
+    public void saveTest(){
+        Book book = new Book();
+        book.setBookName("a");
+        book.setBookAuthor("b");
+        book.setBookType("c");
+        bookService.save(book);
+    }
+    @Test
+    public void findByIdTest(){
+        Book book = bookService.findById(8);
+        System.out.println(book);
+    }
+    @Test
+    public void findAllTest(){
+        List bookList = bookService.findAll();
+        for(int i=0;i<bookList.size();i++){
+            System.out.println(bookList.get(i));
+        }
+    }
+    @Test
+    public void countTest(){
+        int i = bookService.count();
+        System.out.println(i);
+    }
+}
